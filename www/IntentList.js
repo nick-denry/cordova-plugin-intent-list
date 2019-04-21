dc116f0f71bd7b31f5778db70bd8c367f6b33cdf
Nick Denry <nick.denry@gmail.com>
2019-04-21 03:03:31 +0300

    Add comment

--- www/IntentList.js
+++ www/IntentList.js
@@ -1,5 +1,8 @@
 var exec = require('cordova/exec');
 
+/**
+* TODO: add params to configure what info to return with getList
+*/
 exports.getList = function (success, error) {
     exec(success, error, 'IntentList', 'getIntentList', []);
 };
